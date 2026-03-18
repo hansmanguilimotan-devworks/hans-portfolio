d2d63b226c635dd75237fad76e8a06caedef698b
Hans S. Manguilimotan <130669271+hansmanguilimotan-devworks@users.noreply.github.com>
2026-03-18 23:14:27 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,16 @@ export const siteConfig = {
   accentColor: "#1d4ed8",
   social: {
     email: "hansmanguilimotan880@gmail.com",
-    linkedin: "www.linkedin.com/in/hans-manguilimotan-969927298",
+    linkedin: "https://www.linkedin.com/in/hans-manguilimotan-969927298/",
     twitter: "https://x.com/Seiichaaan",
     github: "https://github.com/hansmanguilimotan-devworks",
   },
   aboutMe:
     "With 5 years experience in Crypto Trading since 2021 managing and growing my own portfolio and really into trading. I work in Installers PH also a Tech Company for my OJT as a student, installed cctv's, cabling, accounting works at office managing and tracking the sales receipts, and talk clients on the phone those who inquire in the company for 2months. Manage and track sales also on our own Frozen Store Business in Philippines, Cebu. Graduating of Computer Engineering Course running for Latin Honors this June.",
+  
+  // IMAGE
+  aboutImage: "/images/about-hans.jpg", 
+    
   skills: ["Web Development", "Bookkeeping", "Virtual Assistant", "Admin Tasks"],
   projects: [
     {
@@ -19,6 +23,7 @@ export const siteConfig = {
         "A live website for our kiosk product that provides secure, affordable, and contactless printing, scanning, and copying. By using the website, users can avoid physical contact with USBs. Our automated kiosks support all payment methods, including coins, bills, and online payments.",
       link: "https://kwikdoc.site/",
       skills: ["React", "Express.js", "Vultr"],
+      image: "/images/projects/kwikdoc.png" // Project Image
     },
   ],
   experience: [
@@ -31,6 +36,7 @@ export const siteConfig = {
         "Sales Management and Talked to Clients in office for the Tech Company",
         "Helped in the team in Web Development Projects",
       ],
+       image: "/images/exp/installers-ph.jpg" // Experience Image
     },
   ],
   education: [
@@ -43,6 +49,7 @@ export const siteConfig = {
         "Dean's List all semesters",
         "Participated Regionals Computer Quiz Bowl",
       ],
+      image: "/images/edu/uc-main.jpg" // Education Image
     },
     {
       school: "Online Platform",

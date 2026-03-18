d2ad8261e77f4b5a80189523ac7d9e383ead03cd
Hans S. Manguilimotan <130669271+hansmanguilimotan-devworks@users.noreply.github.com>
2026-03-18 13:53:52 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. ADD THESE TWO LINES FOR GITHUB PAGES
+  site: 'https://hansmanguilimotan-devworks.github.io',
+  base: '/hans-portfolio',
+
   vite: {
     plugins: [tailwindcss()],
   },
